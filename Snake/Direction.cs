2274c01b97d32ad9ecd89ec91dadfff1977e07cd
Casalnovo Giacomo <gc76@live.it>
2023-03-10 15:56:19 +0100

    override operators EQUALS and GETHASHCODE

--- Snake/Direction.cs
+++ Snake/Direction.cs
@@ -1,4 +1,7 @@
-﻿namespace Snake
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
 {
     public class Direction
     {
@@ -25,6 +28,26 @@
             return new Direction(-RowOffset, -ColOffset);
         }
 
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   RowOffset == direction.RowOffset &&
+                   ColOffset == direction.ColOffset;
+        }
 
-    }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColOffset);
+        }
+
+        public static bool operator ==(Direction left, Direction right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction left, Direction right)
+        {
+            return !(left == right);
+        }
+    }//END_Direction
 }
