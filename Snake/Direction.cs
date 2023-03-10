2a2b25390898cf852c5fe01e579e916f04ee3601
Casalnovo Giacomo <gc76@live.it>
2023-03-10 15:15:42 +0100

    add directions

--- Snake/Direction.cs
+++ Snake/Direction.cs
@@ -2,5 +2,22 @@
 {
     public class Direction
     {
+        // Direzioni
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+        
+        public int RowOffset { get; }
+        public int ColOffset { get; }
+
+
+        // costruttore privato per cui non ci saranno instanziazioni da altre classi
+
+        private Direction(int rowOffset, int colOffset)
+        {
+            RowOffset = rowOffset;
+            ColOffset = colOffset;
+        }
     }
 }
